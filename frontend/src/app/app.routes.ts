75e5ebbb6fbacd0d388cbb8eed0f84aeb48e8655
MrKatze <charlycasarin7w7@gmail.com>
2025-03-24 20:51:35 -0600

    Revert "componente blackjack e index"
    This reverts commit 9cf3a7512035bfa664b0734c54e1428d500885ec.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BlackJackComponent } from './pages/blackjack/blackjack.component';
-//import { RouletteComponent } from './pages/roulette/roulette.component';
-//import { SlotsComponent } from './pages/slots/slots.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component'; // Importa el DashboardComponent
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'index', component: DashboardComponent }, // La página principal
-  { path: 'blackjack', component: BlackJackComponent },
- // { path: 'Roulette', component: RouletteComponent },
-// { path: 'Slots', component: SlotsComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/index' } // Redirige cualquier ruta desconocida a index
+  { path: 'dashboard', component: DashboardComponent }, 
 ];
